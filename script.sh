76f3fef60ed1d1ae98ed5ce02c26c1d2c77722cd
harshagr64 <55557377+harshagr64@users.noreply.github.com>
2022-02-04 12:41:11 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 length=$(ls -al /home/harsh | grep pyjenkinsenv | wc -l)
 if [ $length -eq 0 ]
 then
-	echo "Harsh@123" | sudo -u harsh -S python3 -m venv /home/harsh/pyjenkinsenv 
+	python3 -m venv /home/harsh/pyjenkinsenv 
 # && source activate;
 
 else
