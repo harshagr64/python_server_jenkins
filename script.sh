948dd3133dae673789dbc58fd9dc2e4944b3d212
harshagr64 <55557377+harshagr64@users.noreply.github.com>
2022-02-04 17:18:59 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,10 @@
 length=$(ls -al /home/harsh | grep pyjenkinsenv | wc -l)
 if [ $length -eq 0 ]
 then
-	sudo python3 -m venv /home/harsh/pyjenkinsenv && sudo source /home/harsh/pyjenkinsenv/activate
-# && source activate;
+	sudo python3 -m venv /home/harsh/pyjenkinsenv
+	# sudo /home/harsh/pyjenkinsenv/bin/python3 
+	# && source activate;
+	/home/harsh/pyjenkinsenv/bin/python3 first.py
 
 else
 	echo "This is not a directory"
